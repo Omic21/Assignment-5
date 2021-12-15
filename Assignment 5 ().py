b21a8a421ebdeaa9ad25011ee343b07807c257e9
Omic21 <94055478+Omic21@users.noreply.github.com>
2021-12-15 17:13:31 +0530

    Add files via upload

---
+++ Assignment 5 ().py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+class abc:
+
+    def __init__(x, n):
+        return (x**n)
+
+print(abc.__init__(10,2))
+print(abc.__init__(5,2))
+print(abc.__init__(3,3))
+
+
+# In[ ]:
+
+
+
+
